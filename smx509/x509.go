b66f1b467cc7240dc361a5cd7638688082bb6265
Sun Yimin <emmansun@users.noreply.github.com>
2021-06-29 10:37:41 +0800

    Update x509.go
    compatible with 1.4

--- smx509/x509.go
+++ smx509/x509.go
@@ -1889,12 +1889,18 @@ func CreateCertificate(rand io.Reader, template, parent *x509.Certificate, pub, 
 	type privateKey interface {
 		Equal(crypto.PublicKey) bool
 	}
-	if privPub, ok := key.Public().(privateKey); !ok {
-		return nil, errors.New("x509: internal error: supported public key does not implement Equal")
-	} else if parent.PublicKey != nil && !privPub.Equal(parent.PublicKey) {
+	
+	/*
+		if privPub, ok := key.Public().(privateKey); !ok {
+			return nil, errors.New("x509: internal error: supported public key does not implement Equal")
+		} else if parent.PublicKey != nil && !privPub.Equal(parent.PublicKey) {
+			return nil, errors.New("x509: provided PrivateKey doesn't match parent's PublicKey")
+		}
+	*/
+	if privPub, ok := key.Public().(privateKey); ok && parent.PublicKey != nil && !privPub.Equal(parent.PublicKey) {
 		return nil, errors.New("x509: provided PrivateKey doesn't match parent's PublicKey")
 	}
-
+ 
 	extensions, err := buildExtensions(template, bytes.Equal(asn1Subject, emptyASN1Subject), authorityKeyId)
 	if err != nil {
 		return nil, err
