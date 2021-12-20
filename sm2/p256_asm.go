659facc2c4e02cbafc9a722561cf90e4806e664d
Emman <emman.sun@cargosmart.com>
2021-12-20 12:52:51 +0800

    sm2 arm64

--- sm2/p256_asm.go
+++ sm2/p256_asm.go
@@ -7,8 +7,8 @@
 //                          256-bit primes"
 // https://link.springer.com/article/10.1007%2Fs13389-014-0090-x
 // https://eprint.iacr.org/2013/816.pdf
-//go:build amd64
-// +build amd64
+//go:build amd64 || arm64
+// +build amd64 arm64
 
 package sm2
 
