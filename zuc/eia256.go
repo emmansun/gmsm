eb56eac42ac98bd7f9881126b03638e4904d0356
Emman <emman.sun@cargosmart.com>
2022-04-26 10:04:30 +0800

    support zuc eia 256 bits

--- zuc/eia256.go
+++ zuc/eia256.go
@@ -1,41 +1,49 @@
 package zuc
 
-// Just for reference, no performance advantage due to the block size / chunk are 4 bytes only!
-
 import (
 	"encoding/binary"
 	"fmt"
 )
 
-const (
-	chunk = 4
-)
-
-type ZUC128Mac struct {
+type ZUC256Mac struct {
 	zucState32
 	initState zucState32
 	tagSize   int
-	k0        uint32
-	t         uint32
+	k0        []uint32
+	t         []uint32
 	x         [chunk]byte
 	nx        int
 	len       uint64
 }
 
-// NewHash create hash for zuc-128 eia, with arguments key and iv.
-func NewHash(key, iv []byte) (*ZUC128Mac, error) {
+// NewHash create hash for zuc-128 eia, with arguments key, iv and tagSize.
+// The larger the tag size, the worse the performance.
+func NewHash256(key, iv []byte, tagSize int) (*ZUC256Mac, error) {
 	k := len(key)
 	ivLen := len(iv)
-	mac := &ZUC128Mac{}
-	mac.tagSize = 4
+	mac := &ZUC256Mac{}
+	var d []byte
+	switch tagSize {
+	default:
+		return nil, fmt.Errorf("zuc/eia: invalid tag size %d, support 4/8/16 in bytes", tagSize)
+	case 4:
+		d = zuc256_d[0][:]
+	case 8:
+		d = zuc256_d[1][:]
+	case 16:
+		d = zuc256_d[2][:]
+	}
+	mac.tagSize = tagSize
+	mac.t = make([]uint32, mac.tagSize/4)
+	mac.k0 = make([]uint32, mac.tagSize/4)
 	switch k {
 	default:
-		return nil, fmt.Errorf("zuc/eia: invalid key size %d, expect 16 in bytes", k)
-	case 16: // ZUC-128
-		if ivLen != 16 {
-			return nil, fmt.Errorf("zuc/eia: invalid iv size %d, expect 16 in bytes", ivLen)
+		return nil, fmt.Errorf("zuc/eia: invalid key size %d, expect 32 in bytes", k)
+	case 32: // ZUC-256
+		if ivLen != 23 {
+			return nil, fmt.Errorf("zuc/eia: invalid iv size %d, expect 23 in bytes", ivLen)
 		}
-		mac.loadKeyIV16(key, iv)
+		mac.loadKeyIV32(key, iv, d)
 	}
 	// initialization
 	for i := 0; i < 32; i++ {
@@ -57,52 +65,41 @@ func NewHash(key, iv []byte) (*ZUC128Mac, error) {
 	return mac, nil
 }
 
-func genIV4EIA(count, bearer, direction uint32) []byte {
-	iv := make([]byte, 16)
-	binary.BigEndian.PutUint32(iv, count)
-	copy(iv[9:12], iv[1:4])
-	iv[4] = byte(bearer << 3)
-	iv[12] = iv[4]
-	iv[8] = iv[0] ^ byte(direction<<7)
-	iv[14] = byte(direction << 7)
-	return iv
-}
-
-// NewEIAHash create hash for zuc-128 eia, with arguments key, count, bearer and direction
-func NewEIAHash(key []byte, count, bearer, direction uint32) (*ZUC128Mac, error) {
-	return NewHash(key, genIV4EIA(count, bearer, direction))
-}
-
-func (m *ZUC128Mac) Size() int {
+func (m *ZUC256Mac) Size() int {
 	return m.tagSize
 }
 
-func (m *ZUC128Mac) BlockSize() int {
-	return 4
+func (m *ZUC256Mac) BlockSize() int {
+	return chunk
 }
 
-func (m *ZUC128Mac) Reset() {
-	m.k0 = 0
-	m.t = 0
+func (m *ZUC256Mac) Reset() {
 	m.nx = 0
 	m.len = 0
 	m.r1 = m.initState.r1
 	m.r2 = m.initState.r2
 	copy(m.lfsr[:], m.initState.lfsr[:])
-	m.k0 = m.genKeyword()
+	m.genKeywords(m.t)
+	m.genKeywords(m.k0)
 }
 
-func (m *ZUC128Mac) block(p []byte) {
+func (m *ZUC256Mac) block(p []byte) {
 	for len(p) >= chunk {
 		w := binary.BigEndian.Uint32(p)
 		k1 := m.genKeyword()
 
 		for i := 0; i < 32; i++ {
 			if w&0x80000000 == 0x80000000 {
-				m.t ^= m.k0
+				for j := 0; j < m.tagSize/4; j++ {
+					m.t[j] ^= m.k0[j]
+				}
 			}
 			w <<= 1
-			m.k0 = (m.k0 << 1) | (k1 >> 31)
+			var j int
+			for j = 0; j < m.tagSize/4-1; j++ {
+				m.k0[j] = (m.k0[j] << 1) | (m.k0[j+1] >> 31)
+			}
+			m.k0[j] = (m.k0[j] << 1) | (k1 >> 31)
 			k1 <<= 1
 		}
 
@@ -110,7 +107,7 @@ func (m *ZUC128Mac) block(p []byte) {
 	}
 }
 
-func (m *ZUC128Mac) Write(p []byte) (nn int, err error) {
+func (m *ZUC256Mac) Write(p []byte) (nn int, err error) {
 	nn = len(p)
 	m.len += uint64(nn)
 	if m.nx > 0 {
@@ -133,7 +130,7 @@ func (m *ZUC128Mac) Write(p []byte) (nn int, err error) {
 	return
 }
 
-func (m *ZUC128Mac) checkSum(additionalBits int, b byte) [4]byte {
+func (m *ZUC256Mac) checkSum(additionalBits int, b byte) []byte {
 	if m.nx >= 4 {
 		panic("m.nx >= 4")
 	}
@@ -144,25 +141,32 @@ func (m *ZUC128Mac) checkSum(additionalBits int, b byte) [4]byte {
 
 		for i := 0; i < 8*m.nx+additionalBits; i++ {
 			if w&0x80000000 == 0x80000000 {
-				m.t ^= m.k0
+				for j := 0; j < m.tagSize/4; j++ {
+					m.t[j] ^= m.k0[j]
+				}
 			}
 			w <<= 1
-			m.k0 = (m.k0 << 1) | (k1 >> 31)
+			var j int
+			for j = 0; j < m.tagSize/4-1; j++ {
+				m.k0[j] = (m.k0[j] << 1) | (m.k0[j+1] >> 31)
+			}
+			m.k0[j] = (m.k0[j] << 1) | (k1 >> 31)
 			k1 <<= 1
 		}
 	}
-	m.t ^= m.k0
-	k1 := m.genKeyword()
-	m.t ^= k1
 
-	var digest [4]byte
-	binary.BigEndian.PutUint32(digest[:], m.t)
+	digest := make([]byte, m.tagSize)
+	for j := 0; j < m.tagSize/4; j++ {
+		m.t[j] ^= m.k0[j]
+		binary.BigEndian.PutUint32(digest[j*4:], m.t[j])
+	}
+
 	return digest
 }
 
 // Finish this function hash nbits data in p and return mac value
 // In general, we will use byte level function, this is just for test/verify.
-func (m *ZUC128Mac) Finish(p []byte, nbits int) []byte {
+func (m *ZUC256Mac) Finish(p []byte, nbits int) []byte {
 	if len(p) < (nbits+7)/8 {
 		panic("invalid p length")
 	}
@@ -179,7 +183,7 @@ func (m *ZUC128Mac) Finish(p []byte, nbits int) []byte {
 	return digest[:]
 }
 
-func (m *ZUC128Mac) Sum(in []byte) []byte {
+func (m *ZUC256Mac) Sum(in []byte) []byte {
 	// Make a copy of d so that caller can keep writing and summing.
 	d0 := *m
 	hash := d0.checkSum(0, 0)
