936c19772ef3d000ca7320eb34003022de33f643
Sun Yimin <emmansun@users.noreply.github.com>
2022-06-09 09:11:43 +0800

    temp solution for darwin

--- smx509/root_darwin.go
+++ smx509/root_darwin.go
@@ -1,13 +1,9 @@
 package smx509
 
-import (
-	"errors"
-)
-
 func (c *Certificate) systemVerify(opts *VerifyOptions) (chains [][]*Certificate, err error) {
 	return nil, nil
 }
 
 func loadSystemRoots() (*CertPool, error) {
-	return nil, errors.New("x509: gmsm does not support darwin system root yet")
+	return [][]*Certificate{{}}, nil
 }
