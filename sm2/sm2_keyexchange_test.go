0136fdcabf16f9980d08d132671a5fa37f45a141
Sun Yimin <emmansun@users.noreply.github.com>
2022-06-20 09:45:34 +0800

    forgot one file

--- sm2/sm2_keyexchange_test.go
+++ sm2/sm2_keyexchange_test.go
@@ -9,9 +9,14 @@ import (
 func TestKeyExchangeSample(t *testing.T) {
 	priv1, _ := GenerateKey(rand.Reader)
 	priv2, _ := GenerateKey(rand.Reader)
-	initiator := NewKeyExchange(priv1, &priv2.PublicKey, []byte("Alice"), []byte("Bob"), 32, true)
-	responsder := NewKeyExchange(priv2, &priv1.PublicKey, []byte("Bob"), []byte("Alice"), 32, true)
-
+	initiator, err := NewKeyExchange(priv1, &priv2.PublicKey, []byte("Alice"), []byte("Bob"), 32, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	responsder, err := NewKeyExchange(priv2, &priv1.PublicKey, []byte("Bob"), []byte("Alice"), 32, true)
+	if err != nil {
+		t.Fatal(err)
+	}
 	rA, err := initiator.InitKeyExchange(rand.Reader)
 	if err != nil {
 		t.Fatal(err)
