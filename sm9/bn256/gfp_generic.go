2da0a9cebce6296fc39fbe7fad07def49d0d9c1d
Sun Yimin <emmansun@users.noreply.github.com>
2023-06-21 09:02:09 +0800

    sm9/bn256: add comment for gfpNeg

--- sm9/bn256/gfp_generic.go
+++ sm9/bn256/gfp_generic.go
@@ -30,7 +30,7 @@ func gfpNeg(c, a *gfP) {
 	for i, pi := range p2 {
 		c[i], carry = bits.Sub64(pi, a[i], carry)
 	}
-	// really required
+	// required for "zero", bn256 treat infinity point as valid
 	gfpCarry(c, 0)
 }
 
