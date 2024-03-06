8fc2696d4604b5be6d6689fc86af945151b64f52
甲鲲 <jykaxy@gmail.com>
2024-03-06 21:21:25 +0800

    fixbug: Apple Silicon build fail.

--- internal/cpuid/cpuid_arm64.go
+++ internal/cpuid/cpuid_arm64.go
@@ -1,6 +1,20 @@
 package cpuid
 
-import "golang.org/x/sys/cpu"
+import (
+	"runtime"
 
-var HasAES = cpu.ARM64.HasAES
-var HasGFMUL = cpu.ARM64.HasPMULL
+	"golang.org/x/sys/cpu"
+)
+
+var HasAES = false
+var HasGFMUL = false
+
+func init() {
+	if runtime.GOOS == "darwin/arm64" {
+		HasAES = true
+		HasGFMUL = true
+	} else {
+		HasAES = cpu.ARM64.HasAES
+		HasGFMUL = cpu.ARM64.HasPMULL
+	}
+}
