335947eb5ac26ee8a7fe74029b9e453115644c87
Sun Yimin <emmansun@users.noreply.github.com>
2024-09-03 16:16:56 +0800

    debug 4

--- sm3/sm3blocks_s390x_test.go
+++ sm3/sm3blocks_s390x_test.go
@@ -21,6 +21,7 @@ func TestTransposeMatrix(t *testing.T) {
 	input := [4]*[8]uint32{&m[0], &m[1], &m[2], &m[3]}
 	transposeMatrix(&input[0])
 	fmt.Println()
+	fmt.Println()
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 8; j++ {
 			m[i][j] = uint32(i*4 + j)
