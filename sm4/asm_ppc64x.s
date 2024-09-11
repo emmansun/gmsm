73243191f3c4c20f5ade1b3a5a061d790630d854
Sun Yimin <emmansun@users.noreply.github.com>
2024-09-11 11:13:25 +0800

    sm4: ppc4x, test sm4 box

--- sm4/asm_ppc64x.s
+++ sm4/asm_ppc64x.s
@@ -183,6 +183,11 @@ TEXT ·expandKeyAsm(SB),NOSPLIT,$0
 	VSLDOI $4, V1, V1, V2
 	VSLDOI $4, V2, V2, V3
 
+	VOR ESPERMW, ESPERMW, V5
+	SM4_BOX(V5, V6, V7)
+	STXVW4X V5, (R5)
+
+/*
 ksLoop:
 	LXVW4X (R4), V4
 	SM4_EXPANDKEY_ROUND(V4, V7, V8, V9, V0, V1, V2, V3, V5)
@@ -200,7 +205,7 @@ ksLoop:
 	ADD $16, R4
 	ADD $-16, R6
 	BDNZ	ksLoop
-
+*/
     RET
 
 // func encryptBlocksAsm(xk *uint32, dst, src []byte, inst int)
