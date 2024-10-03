a193c5dec0bc941ccd084c2da96a67514cba56c0
Sun Yimin <emmansun@users.noreply.github.com>
2024-10-03 13:45:17 +0800

    zuc: ppc64x, check r1 r2

--- zuc/core_ppc64x_test.go
+++ zuc/core_ppc64x_test.go
@@ -14,6 +14,12 @@ func Test_genKeywordAsm_case1(t *testing.T) {
 	if z1 != 0x27bede74 {
 		t.Errorf("expected=%x, result=%x\n", 0x27bede74, z1)
 	}
+	if s.r1 != 0xc7ee7f13 {
+		t.Errorf("expected=%x, result=%x\n", 0xc7ee7f13, s.r1)
+	}
+	if s.r2 != 0xc0fa817 {
+		t.Errorf("expected=%x, result=%x\n", 0xc0fa817, s.r2)
+	}	
 	z2 := genKeywordAsm(s)
 	if z2 != 0x018082da {
 		t.Errorf("expected=%x, result=%x\n", 0x018082da, z2)
