3c24ac0690e79fca186754c9be68504d4a3bff80
Sun Yimin <emmansun@users.noreply.github.com>
2025-05-27 10:14:54 +0800

    slhdsa: go1.24 build

--- slhdsa/parameterset.go
+++ slhdsa/parameterset.go
@@ -1,3 +1,9 @@
+// Copyright 2025 Sun Yimin. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+//go:build go1.24
+
 package slhdsa
 
 import "io"
