b3b25ac091fc1aafb5dac3ef721a46bda28fe5e6
Sun Yimin <emmansun@users.noreply.github.com>
2025-10-22 10:17:49 +0800

    test riscv64 block

--- internal/sm3/sm3block_riscv64_test.go
+++ internal/sm3/sm3block_riscv64_test.go
@@ -9,6 +9,8 @@ package sm3
 import (
 	"fmt"
 	"testing"
+
+	"github.com/emmansun/gmsm/internal/byteorder"
 )
 
 func TestBlocktest(t *testing.T) {
@@ -18,4 +20,26 @@ func TestBlocktest(t *testing.T) {
 	}
 	blocktest(data)
 	fmt.Printf("%x", data)
+
+	clear(data)
+	data[0] = 'a'
+	data[1] = 'b'
+	data[2] = 'c'
+	data[3] = 0x80
+	data[63] = 0x18
+
+	d := New()
+	d1, _ := d.(*digest)
+	block(d1, data)
+	var digest [Size]byte
+
+	byteorder.BEPutUint32(digest[0:], d1.h[0])
+	byteorder.BEPutUint32(digest[4:], d1.h[1])
+	byteorder.BEPutUint32(digest[8:], d1.h[2])
+	byteorder.BEPutUint32(digest[12:], d1.h[3])
+	byteorder.BEPutUint32(digest[16:], d1.h[4])
+	byteorder.BEPutUint32(digest[20:], d1.h[5])
+	byteorder.BEPutUint32(digest[24:], d1.h[6])
+	byteorder.BEPutUint32(digest[28:], d1.h[7])
+	fmt.Printf("\n%x", digest)
 }
