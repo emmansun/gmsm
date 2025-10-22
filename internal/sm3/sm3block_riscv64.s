70b4b61439e174f2a2caa958730615c3626c1cd0
Sun Yimin <emmansun@users.noreply.github.com>
2025-10-22 17:11:51 +0800

    internal/sm3: it seems the value in X31 will be changed

--- internal/sm3/sm3block_riscv64.s
+++ internal/sm3/sm3block_riscv64.s
@@ -33,8 +33,8 @@
 #define BX X26
 #define CX X28
 #define DX X29
-#define hlp0 X30 // it seems we can't use X31 here, so we reused X7
-#define REG_KT X31
+#define hlp0 X7 // it seems we can't use X31 here, so we reused X7
+#define REG_KT X30
 
 #define stackaddress(index) ((index)*4+8)(RSP) // for RISCV64 stack usage, we CAN NOT overwrite the first 8 bytes space!
 
@@ -177,7 +177,7 @@ TEXT ·block(SB), 0, $280-32
 
 	MOV	$·_K(SB), REG_KT		// const table
 
-	ADD X6, X7, REG_END_ADDR
+	ADD X6, X7, REG_END_ADDR    // X7 will be reused as a temp register
 
 	MOVW	(0*4)(X5), REG_A
 	MOVW	(1*4)(X5), REG_B
