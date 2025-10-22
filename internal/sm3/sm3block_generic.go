9a7880fbc56d0aaca40f52961c61ecfcdd897c5e
Sun Yimin <emmansun@users.noreply.github.com>
2025-10-22 15:07:14 +0800

    internal/sm3: forgot go:build change

--- internal/sm3/sm3block_generic.go
+++ internal/sm3/sm3block_generic.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-//go:build purego || !(amd64 || arm64 || ppc64 || ppc64le || s390x)
+//go:build purego || !(amd64 || arm64 || ppc64 || ppc64le || s390x || loong64 || riscv64)
 
 package sm3
 
