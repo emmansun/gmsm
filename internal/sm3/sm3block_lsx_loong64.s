edf0d441355f9aea7bd92b8faf56a66118d8be35
Sun Yimin <emmansun@users.noreply.github.com>
2025-10-24 08:56:00 +0800

    try VILVHV

--- internal/sm3/sm3block_lsx_loong64.s
+++ internal/sm3/sm3block_lsx_loong64.s
@@ -56,9 +56,8 @@
 	VMOVQ XWORD0.W[3], hlp0          \
 	VMOVQ hlp0, XTMP0.W[0]           \ // XTMP0 = W[-13] = {w6,w5,w4,w3}
 	VROTRW $(32-7), XTMP0, XTMP1     \ // XTMP1 = W[-13] rol 7
-	VSHUF4IW $0x40, XWORD3, XTMP0    \
-	VMOVQ XWORD2.V[1], hlp0          \
-	VMOVQ hlp0, XTMP0.V[0]           \ // XTMP0 = W[-6] = {w13,w12,w11,w10}
+	VSHUF4IW $0x44, XWORD3, XTMP0    \
+	VILVHV XTMP0, XWORD2, XTMP0      \ // XTMP0 = W[-6] = {w13,w12,w11,w10}
 	VXORV XTMP1, XTMP0, XTMP0        \ // XTMP0 = W[-6] ^ (W[-13] rol 7)
 	\ // Prepare P1 parameters
 	VSHUF4IW $0x90, XWORD2, XTMP1    \
