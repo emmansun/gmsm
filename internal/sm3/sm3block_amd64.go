1040bcfc6dce50cbfdea8749b60e74bee07ea89a
Sun Yimin <emmansun@users.noreply.github.com>
2025-12-04 11:20:42 +0800

    internal/sm3: remove AVX optimization #413

--- internal/sm3/sm3block_amd64.go
+++ internal/sm3/sm3block_amd64.go
@@ -5,11 +5,7 @@ package sm3
 import "github.com/emmansun/gmsm/internal/deps/cpu"
 
 var useAVX2 = cpu.X86.HasAVX2 && cpu.X86.HasBMI2
-var useAVX = cpu.X86.HasAVX
 var useSSSE3 = cpu.X86.HasSSSE3
-
-//go:noescape
-func blockAMD64(dig *digest, p []byte)
 
 //go:noescape
 func blockSIMD(dig *digest, p []byte)
@@ -21,9 +17,9 @@ func block(dig *digest, p []byte) {
 	switch {
 	case useAVX2:
 		blockAVX2(dig, p)
-	case useSSSE3, useAVX: // useSSSE3 or useAVX
+	case useSSSE3:
 		blockSIMD(dig, p)
 	default:
-		blockAMD64(dig, p)
+		blockGeneric(dig, p)
 	}
 }
