b2752c45e8459beedb2d56b54a8459a3d690786f
Sun Yimin <emmansun@users.noreply.github.com>
2025-12-05 09:56:23 +0800

    internal/bigmod: use go sdk's global asm function

--- internal/bigmod/nat_arm.s
+++ internal/bigmod/nat_arm.s
@@ -9,44 +9,19 @@
 // func addMulVVW256(z, x *uint, y uint) (c uint)
 TEXT ·addMulVVW256(SB), $0-16
 	MOVW	$8, R5
-	JMP		addMulVVWy(SB)
+	JMP		addMulVVWx(SB)
 
 // func addMulVVW1024(z, x *uint, y uint) (c uint)
 TEXT ·addMulVVW1024(SB), $0-16
 	MOVW	$32, R5
-	JMP		addMulVVWy(SB)
+	JMP		addMulVVWx(SB)
 
 // func addMulVVW1536(z, x *uint, y uint) (c uint)
 TEXT ·addMulVVW1536(SB), $0-16
 	MOVW	$48, R5
-	JMP		addMulVVWy(SB)
+	JMP		addMulVVWx(SB)
 
 // func addMulVVW2048(z, x *uint, y uint) (c uint)
 TEXT ·addMulVVW2048(SB), $0-16
 	MOVW	$64, R5
-	JMP		addMulVVWy(SB)
-
-TEXT addMulVVWy(SB), NOFRAME|NOSPLIT, $0
-	MOVW	$0, R0
-	MOVW	z+0(FP), R1
-	MOVW	x+4(FP), R2
-	MOVW	y+8(FP), R3
-	ADD	R5<<2, R1, R5
-	MOVW	$0, R4
-	B E9
-
-L9:	MOVW.P	4(R2), R6
-	MULLU	R6, R3, (R7, R6)
-	ADD.S	R4, R6
-	ADC	R0, R7
-	MOVW	0(R1), R4
-	ADD.S	R4, R6
-	ADC	R0, R7
-	MOVW.P	R6, 4(R1)
-	MOVW	R7, R4
-
-E9:	TEQ	R1, R5
-	BNE	L9
-
-	MOVW	R4, c+12(FP)
-	RET
+	JMP		addMulVVWx(SB)
