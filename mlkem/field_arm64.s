ebc4766e48225edef0acd278199665b5b230106f
Sun Yimin <emmansun@users.noreply.github.com>
2026-04-23 16:45:40 +0800

    mlkem: fix rejUniformARM64 again #479

--- mlkem/field_arm64.s
+++ mlkem/field_arm64.s
@@ -1285,7 +1285,8 @@ TEXT ·rejUniformARM64(SB), NOSPLIT, $0-48
 	MOVD j+32(FP), R3
 	MOVD R3, R4
 
-	CMP $256, R3
+	MOVD $256, R24
+	CMP R3, R24
 	BGE rejuniform_arm64_done
 	CMP $24, R1
 	BNE rejuniform_arm64_loop_setup
@@ -1295,6 +1296,7 @@ TEXT ·rejUniformARM64(SB), NOSPLIT, $0-48
 	MOVD $3328, R25
 	VDUP R25, V31.H8
 	MOVD $0xFF, R25
+	MOVD $256, R26
 
 	// Fast block 0: bytes [0..11] -> eight 12-bit candidates.
 	MOVD (R0), R6
@@ -1325,27 +1327,43 @@ TEXT ·rejUniformARM64(SB), NOSPLIT, $0-48
 
 	TBZ $0, R22, rejuniform_arm64_fast0_skip0
 	STORE_REJ_COEFF(R10)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast0_skip0:
 	TBZ $1, R22, rejuniform_arm64_fast0_skip1
 	STORE_REJ_COEFF(R11)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast0_skip1:
 	TBZ $2, R22, rejuniform_arm64_fast0_skip2
 	STORE_REJ_COEFF(R12)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast0_skip2:
 	TBZ $3, R22, rejuniform_arm64_fast0_skip3
 	STORE_REJ_COEFF(R13)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast0_skip3:
 	TBZ $4, R22, rejuniform_arm64_fast0_skip4
 	STORE_REJ_COEFF(R14)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast0_skip4:
 	TBZ $5, R22, rejuniform_arm64_fast0_skip5
 	STORE_REJ_COEFF(R15)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast0_skip5:
 	TBZ $6, R22, rejuniform_arm64_fast0_skip6
 	STORE_REJ_COEFF(R16)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast0_skip6:
 	TBZ $7, R22, rejuniform_arm64_fast0_skip7
 	STORE_REJ_COEFF(R17)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast0_skip7:
 
 	// Fast block 1: bytes [12..23] -> eight 12-bit candidates.
@@ -1377,33 +1395,50 @@ rejuniform_arm64_fast0_skip7:
 
 	TBZ $0, R22, rejuniform_arm64_fast1_skip0
 	STORE_REJ_COEFF(R10)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast1_skip0:
 	TBZ $1, R22, rejuniform_arm64_fast1_skip1
 	STORE_REJ_COEFF(R11)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast1_skip1:
 	TBZ $2, R22, rejuniform_arm64_fast1_skip2
 	STORE_REJ_COEFF(R12)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast1_skip2:
 	TBZ $3, R22, rejuniform_arm64_fast1_skip3
 	STORE_REJ_COEFF(R13)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast1_skip3:
 	TBZ $4, R22, rejuniform_arm64_fast1_skip4
 	STORE_REJ_COEFF(R14)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast1_skip4:
 	TBZ $5, R22, rejuniform_arm64_fast1_skip5
 	STORE_REJ_COEFF(R15)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast1_skip5:
 	TBZ $6, R22, rejuniform_arm64_fast1_skip6
 	STORE_REJ_COEFF(R16)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast1_skip6:
 	TBZ $7, R22, rejuniform_arm64_fast1_skip7
 	STORE_REJ_COEFF(R17)
+	CMP R3, R26
+	BGE rejuniform_arm64_done
 rejuniform_arm64_fast1_skip7:
 	B rejuniform_arm64_done
 
 rejuniform_arm64_loop_setup:
 	MOVD $0, R5
 	MOVD $3329, R25
+	MOVD $256, R26
 
 rejuniform_arm64_loop:
 	CMP R1, R5
@@ -1418,7 +1453,7 @@ rejuniform_arm64_loop:
 	CMP R8, R25
 	BGE rejuniform_arm64_skip_d1
 	STORE_REJ_COEFF(R8)
-	CMP $256, R3
+	CMP R3, R26
 	BGE rejuniform_arm64_done
 
 rejuniform_arm64_skip_d1:
@@ -1430,7 +1465,7 @@ rejuniform_arm64_skip_d1:
 	CMP R6, R25
 	BGE rejuniform_arm64_next
 	STORE_REJ_COEFF(R6)
-	CMP $256, R3
+	CMP R3, R26
 	BGE rejuniform_arm64_done
 
 rejuniform_arm64_next:
