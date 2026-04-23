567df3f2f1d7a5b719942d4f37b76bb18e4c4b0b
Sun Yimin <emmansun@users.noreply.github.com>
2026-04-23 16:40:03 +0800

    mlkem: fix rejUniformARM64 #479

--- mlkem/field_arm64.s
+++ mlkem/field_arm64.s
@@ -1403,6 +1403,7 @@ rejuniform_arm64_fast1_skip7:
 
 rejuniform_arm64_loop_setup:
 	MOVD $0, R5
+	MOVD $3329, R25
 
 rejuniform_arm64_loop:
 	CMP R1, R5
@@ -1414,7 +1415,7 @@ rejuniform_arm64_loop:
 	LSL $8, R7, R7
 	ORR R7, R6, R6
 	AND $0x0FFF, R6, R8
-	CMP $3329, R8
+	CMP R8, R25
 	BGE rejuniform_arm64_skip_d1
 	STORE_REJ_COEFF(R8)
 	CMP $256, R3
@@ -1426,7 +1427,7 @@ rejuniform_arm64_skip_d1:
 	LSL $8, R7, R7
 	ORR R7, R6, R6
 	LSR $4, R6, R6
-	CMP $3329, R6
+	CMP R6, R25
 	BGE rejuniform_arm64_next
 	STORE_REJ_COEFF(R6)
 	CMP $256, R3
