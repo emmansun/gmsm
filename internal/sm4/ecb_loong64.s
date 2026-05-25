04d65aaee225e57965a6c715ee7617db0486ec48
Sun Yimin <emmansun@users.noreply.github.com>
2026-05-25 17:01:47 +0800

    internal/sm4: rename #505

--- internal/sm4/ecb_loong64.s
+++ internal/sm4/ecb_loong64.s
@@ -1,4 +1,4 @@
-// Copyright 2025 Sun Yimin. All rights reserved.
+// Copyright 2026 Sun Yimin. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
