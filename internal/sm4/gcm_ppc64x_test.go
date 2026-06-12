735f3d9b38c48c58c9fd1136d94a999be1212cae
Sun Yimin <emmansun@users.noreply.github.com>
2026-06-12 17:28:15 +0800

    internal/sm4: add TestGcmSm4Init

--- internal/sm4/gcm_ppc64x_test.go
+++ internal/sm4/gcm_ppc64x_test.go
@@ -7,6 +7,8 @@
 package sm4
 
 import (
+	"bytes"
+	"encoding/hex"
 	"fmt"
 	"runtime"
 	"testing"
@@ -74,4 +76,38 @@ var table = [16]string{
 	"00000000000000000000000000000000",
 	"00000000000000000000000000000000",
 	"00000000000000000000000000000000",
+}
+
+func TestGcmSm4Init(t *testing.T) {
+	var productTable [256]byte
+	key := [16]byte{0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10}
+	expected, _ := hex.DecodeString("efe02875211f104b6cc6398a88e02616832611ffa9ff365d832611ffa9ff365dd1990739ba1568a7b850c2b3d6faa70269c9c58a6cefcfa569c9c58a6cefcfa5c465caca557f2b72b1a41462debd1b0075c1dea88bc2307275c1dea88bc2307285f658150945b9720030ab912a73b71c85c6f38423360e6e85c6f38423360e6e70d7d26d60ba5e2e434c4acffae2f15b339b98a29a58af75339b98a29a58af75edeb6cd41b6c866aa116a5ff33dcbbc04cfdc92b28b03daa4cfdc92b28b03daabf7c2d4efddd55771c7e73c7aa8b732fa3025e8957562658a3025e89575626585444a9b72066aa2e99458213d6e8ef4ccd012ba4f68e4562cd012ba4f68e4562")
+	generateProductTable(t, key[:], &productTable)
+	if !bytes.Equal(productTable[:], expected) {
+		t.Errorf("unexpected table value: got %x, want %x", table, expected)
+	}
+}
+
+func generateProductTable(t *testing.T, key []byte, table *[256]byte) {
+	t.Helper()
+	c, err := newCipherGeneric(key[:])
+	if err != nil {
+		t.Fatal(err)
+	}
+	hle := make([]byte, gcmBlockSize)
+	c.Encrypt(hle, hle)
+
+	// Reverse the bytes in each 8 byte chunk
+	// Load little endian, store big endian
+	var h1, h2 uint64
+	if runtime.GOARCH == "ppc64le" {
+		h1 = byteorder.LEUint64(hle[:8])
+		h2 = byteorder.LEUint64(hle[8:])
+	} else {
+		h1 = byteorder.BEUint64(hle[:8])
+		h2 = byteorder.BEUint64(hle[8:])
+	}
+	byteorder.BEPutUint64(hle[:8], h1)
+	byteorder.BEPutUint64(hle[8:], h2)
+	gcmInit(table, hle)
 }
