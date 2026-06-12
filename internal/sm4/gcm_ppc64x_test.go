cd95357f91b0d4a25761db602d284544b3b8827c
Sun Yimin <emmansun@users.noreply.github.com>
2026-06-12 17:30:45 +0800

    internal/sm4: fix output message issue

--- internal/sm4/gcm_ppc64x_test.go
+++ internal/sm4/gcm_ppc64x_test.go
@@ -84,7 +84,7 @@ func TestGcmSm4Init(t *testing.T) {
 	expected, _ := hex.DecodeString("efe02875211f104b6cc6398a88e02616832611ffa9ff365d832611ffa9ff365dd1990739ba1568a7b850c2b3d6faa70269c9c58a6cefcfa569c9c58a6cefcfa5c465caca557f2b72b1a41462debd1b0075c1dea88bc2307275c1dea88bc2307285f658150945b9720030ab912a73b71c85c6f38423360e6e85c6f38423360e6e70d7d26d60ba5e2e434c4acffae2f15b339b98a29a58af75339b98a29a58af75edeb6cd41b6c866aa116a5ff33dcbbc04cfdc92b28b03daa4cfdc92b28b03daabf7c2d4efddd55771c7e73c7aa8b732fa3025e8957562658a3025e89575626585444a9b72066aa2e99458213d6e8ef4ccd012ba4f68e4562cd012ba4f68e4562")
 	generateProductTable(t, key[:], &productTable)
 	if !bytes.Equal(productTable[:], expected) {
-		t.Errorf("unexpected table value: got %x, want %x", table, expected)
+		t.Errorf("unexpected table value: got %x, want %x", productTable, expected)
 	}
 }
 
