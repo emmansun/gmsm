6470fd41c59de12ba87805b8745789114a844c58
Sun Yimin <emmansun@users.noreply.github.com>
2026-09-09 13:24:12 +0800

    internal/sm4: fallback riscv64 gcm data to amd64 style

--- internal/sm4/gcm_zvksed_riscv64.s
+++ internal/sm4/gcm_zvksed_riscv64.s
@@ -96,25 +96,24 @@ TEXT ·gcmSm4Init(SB),NOSPLIT,$0
 	VLE32V (X12), V3
 	VANDVX X15, V3, V3
 	VORVV V2, V3, V1
-	
+
 	// Now prepare powers of H and pre-computations for them
 	VSETIVLI	$2, E64, M1, TA, MA, X0
 	VIDV V10               // V10 = [0, 1]
 	VRSUBVI $1, V10, V10    // V10 = [1, 0]
 
 	// Karatsuba pre-computations
-	ADD $224, dst, X14
 	VRGATHERVV V10, V1, V2
-	VSE64V V2, (X14)
 	VXORVV V1, V2, V2
-	ADD $16, X14, X14
+
+	ADD $240, dst, X14
 	VSE64V V2, (X14)
+	SUB $16, X14, X14
+	VSE64V V1, (X14)
 
 	MOV gcmPoly<>+0x08(SB), X15
 	VMVVV V1, V3
 	VMVVV V2, V4
-
-	ADD $16, dst, X13	
 
 initLoop:
 		VCLMULVV V1, V3, V5    // LOW(V1 * V3) = [C0, D0]
@@ -149,14 +148,15 @@ initLoop:
 		VXORVV V3, V4, V5
 		VXORVV V5, V8, V3
 
-		SUB $48, X14, X14
 		VRGATHERVV V10, V3, V4
-		VSE64V V4, (X14)
 		VXORVV V3, V4, V4
-		ADD $16, X14, X14
-		VSE64V V4, (X14)
 
-	BNE X13, X14, initLoop
+		SUB $16, X14, X14
+		VSE64V V4, (X14)
+		SUB $16, X14, X14
+		VSE64V V3, (X14)
+
+	BNE dst, X14, initLoop
 	RET
 
 // func gcmSm4Data(productTable *[256]byte, data []byte, T *[16]byte)
@@ -166,25 +166,10 @@ TEXT ·gcmSm4Data(SB),NOSPLIT,$0
 #define tPtr X12
 #define autLen X13
 
-// a remains in swapped-domain.
-//
-// T0     = low64(XPOLY * a)
-// ACCML  = high64(XPOLY * a)
-// ACCMH  = [high(a[1] * XPOLY), low(a[1] * XPOLY)]
-//
-// ACCML and ACCMH are scratch registers after product assembly.
-#define reduceRound(a) \
-	VCLMULVX XPOLY, a, T0; \
-	VCLMULHVX XPOLY, a, ACCML; \
-	VSLIDEDOWNVI $1, ACCML, ACCMH; \
-	VSLIDEDOWNVI $1, T0, ACCML; \
-	VSLIDEUPVI $1, ACCML, ACCMH; \
-	VRGATHERVV SHUFFLE_MASK, a, T0; \
-	VXORVV ACCMH, T0, a
-
+#define reduceRound(a) VCLMULVX XPOLY, a, T0; VCLMULHVX XPOLY, a, ACCMH; VSLIDEUPVI $1, ACCMH, T0; VRGATHERVV SHUFFLE_MASK, a, ACCMH; VXORVV T0, ACCMH, a
 #define mulRoundAAD(X ,i) \
-	VREV8V X, X; \
-	VRGATHERVV SHUFFLE_MASK, X, T0; \
+	VREV8V X, T0; \
+	VRGATHERVV SHUFFLE_MASK, T0, X; \
 	VXORVV X, T0, T0; \
 	ADD $(16*(i*2)), pTbl, X14; \
 	VLE64V (X14), T1; \
@@ -232,7 +217,7 @@ dataTLS:
 	VSLIDEUPVI $1, B1, B0
 	XOR autLen, autLen
 	JMP dataMul
-	
+
 dataOctaLoop:
 		BLT autLen, X8, startSinglesLoop
 		SUB $128, autLen, autLen
@@ -254,10 +239,10 @@ dataOctaLoop:
 		VLE64V (aut), B7
 		ADD $16, aut, aut
 
-		VREV8V B0, B0
+		VREV8V B0, T0
+		VRGATHERVV SHUFFLE_MASK, T0, B0
 		VXORVV ACC0, B0, B0
 
-		// Karatsuba middle operand.
 		VRGATHERVV SHUFFLE_MASK, B0, T0
 		VXORVV B0, T0, T0
 
@@ -265,8 +250,8 @@ dataOctaLoop:
 		ADD $16, pTbl, X9
 		VLE64V (X9), T2
 
-		VCLMULVV B0, T1, ACC0       // LOW(B0 * T1) = [D0, C0]
-		VCLMULHVV B0, T1, ACC1      // HIGH(B0 * T1) = [D1, C1]
+		VCLMULVV B0, T1, ACC0       // LOW(B0 * T1) = [C0, D0]
+		VCLMULHVV B0, T1, ACC1      // HIGH(B0 * T1) = [C1, D1]
 		VCLMULVV T0, T2, ACCML      // LOW(T0 * T2) = [E0, F0]
 		VCLMULHVV T0, T2, ACCMH     // HIGH(T0 * T2) = [E1, F1]
 
@@ -278,16 +263,18 @@ dataOctaLoop:
 		mulRoundAAD(B6, 6)
 		mulRoundAAD(B7, 7)
 
-		VXORVV ACC0, ACC1, T0       // [D0 ^ D1, C0 ^ C1]
-		VXORVV T0, ACCML, ACCML     // [D0 ^ D1 ^ E0, C0 ^ C1 ^ E0]
-		VXORVV T0, ACCMH, ACCMH     // [D0 ^ D1 ^ E1, C0 ^ C1 ^ E1]
+		VXORVV ACC0, ACC1, T0       // [C0 ^ C1, D0 ^ D1]
+		VXORVV T0, ACCML, ACCML     // [C0 ^ C1 ^ E0, D0 ^ D1 ^ F0]
+		VSLIDEDOWNVI $1, T0, T0     // [D0 ^ D1, 0]
+		VXORVV T0, ACCMH, ACCMH     // [D0 ^ D1 ^ E1, *]
 
-		VSLIDEDOWNVI $1, ACCML, T0  // T0 = [C0 ^ C1 ^ E0, 0]
-		VXORVV T0, ACC0, ACC0       // ACC0 = [D0 ^ C0 ^ C1 ^ E0, C0]
+		VSLIDEDOWNVI $1, ACC0, T0   // [D1, 0]
+		VXORVV T0, ACCML, ACCML     // [C0 ^ C1 ^ E0 ^ D1, *]
+		VSLIDEUPVI $1, ACCML, ACC0	// [C0, C0 ^ C1 ^ E0 ^ D1]
 
-		VSLIDEDOWNVI $1, ACC1, T0   // T0 = [C1, 0]
-		VXORVV T0, ACCMH, ACCMH     // ACCMH = [C1 ^ D0 ^ D1 ^ E1, C0 ^ E1]
-		VSLIDEUPVI $1, ACCMH, ACC1  // ACC1 = [D1, C1 ^ D0 ^ D1 ^ E1]
+		VSLIDEDOWNVI $1, ACC1, T0   // [D1, 0]
+		VXORVV ACC1, ACCMH, ACC1    // [C1 ^ D0 ^ D1 ^ E1, *]
+		VSLIDEUPVI $1, T0, ACC1     // [C1 ^ D0 ^ D1 ^ E1, D1] 
 
 		// Fast reduction
 		// 1st reduction
@@ -312,27 +299,30 @@ dataSinglesLoop:
 		ADD $16, aut, aut
 
 dataMul:
-		VREV8V B0, B0
-		VXORVV ACC0, B0, B0        // ACC0 is also maintained in swapped-domain.
+		VREV8V B0, T0
+		VRGATHERVV SHUFFLE_MASK, T0, B0
+		VXORVV ACC0, B0, B0
 
 		VRGATHERVV SHUFFLE_MASK, B0, T0
 		VXORVV B0, T0, T0
 
-		VCLMULVV B0, T1, ACC0       // LOW(B0 * T1) = [D0, C0]
-		VCLMULHVV B0, T1, ACC1      // HIGH(B0 * T1) = [D1, C1]
-		VCLMULVV T0, T2, ACCML      // LOW(T0 * T2) = [E0, F0], E0 == F0
-		VCLMULHVV T0, T2, ACCMH     // HIGH(T0 * T2) = [E1, F1], E1 == F1
+		VCLMULVV B0, T1, ACC0       // LOW(B0 * T1) = [C0, D0]
+		VCLMULHVV B0, T1, ACC1      // HIGH(B0 * T1) = [C1, D1]
+		VCLMULVV T0, T2, ACCML      // LOW(T0 * T2) = [E0, F0]
+		VCLMULHVV T0, T2, ACCMH     // HIGH(T0 * T2) = [E1, F1]
 
-		VXORVV ACC0, ACC1, T0       // [D0 ^ D1, C0 ^ C1]
-		VXORVV T0, ACCML, ACCML     // [D0 ^ D1 ^ E0, C0 ^ C1 ^ E0]
-		VXORVV T0, ACCMH, ACCMH     // [D0 ^ D1 ^ E1, C0 ^ C1 ^ E1]
+		VXORVV ACC0, ACC1, T0       // [C0 ^ C1, D0 ^ D1]
+		VXORVV T0, ACCML, ACCML     // [C0 ^ C1 ^ E0, D0 ^ D1 ^ F0]
+		VSLIDEDOWNVI $1, T0, T0     // [D0 ^ D1, 0]
+		VXORVV T0, ACCMH, ACCMH     // [D0 ^ D1 ^ E1, *]
 
-		VSLIDEDOWNVI $1, ACCML, T0  // T0 = [C0 ^ C1 ^ E0, 0]
-		VXORVV T0, ACC0, ACC0       // ACC0 = [D0 ^ C0 ^ C1 ^ E0, C0]
+		VSLIDEDOWNVI $1, ACC0, T0   // [D0, 0]
+		VXORVV T0, ACCML, ACCML     // [C0 ^ C1 ^ E0 ^ D0, *]
+		VSLIDEUPVI $1, ACCML, ACC0	// [C0, C0 ^ C1 ^ E0 ^ D0]
 
-		VSLIDEDOWNVI $1, ACC1, T0   // T0 = [C1, 0]
-		VXORVV T0, ACCMH, ACCMH     // ACCMH = [C1 ^ D0 ^ D1 ^ E1, C0 ^ E1]
-		VSLIDEUPVI $1, ACCMH, ACC1  // ACC1 = [D1, C1 ^ D0 ^ D1 ^ E1]
+		VSLIDEDOWNVI $1, ACC1, T0   // [D1, 0]
+		VXORVV ACC1, ACCMH, ACC1    // [C1 ^ D0 ^ D1 ^ E1, *]
+		VSLIDEUPVI $1, T0, ACC1     // [C1 ^ D0 ^ D1 ^ E1, D1] 
 
 		// Fast reduction
 		// 1st reduction
@@ -377,7 +367,7 @@ dataLoadLoopHigh8:
 		SUB $1, autLen, autLen
 		ADD $1, aut, aut
 		ADD $8, X14, X14
-	
+
 	JMP dataLoadLoopHigh8
 
 dataLoadDone:
@@ -386,7 +376,7 @@ dataLoadDone:
 	VSLIDEUPVI $1, B1, B0
 
 	JMP dataMul
-	
+
 dataBail:
 	VSE64V ACC0, (tPtr)
 	RET
@@ -394,7 +384,7 @@ dataBail:
 // func gcmSm4Finish(productTable *[256]byte, tagMask, T *[16]byte, pLen, dLen uint64)
 TEXT ·gcmSm4Finish(SB),NOSPLIT,$0
 	RET
-	
+
 // func gcmSm4Enc(productTable *[256]byte, dst, src []byte, ctr, T *[16]byte, rk []uint32)
 TEXT ·gcmSm4Enc(SB),0,$256-96
 	RET
--- internal/sm4/sm4_gcm_asm_test.go
+++ internal/sm4/sm4_gcm_asm_test.go
@@ -40,11 +40,11 @@ func TestGcmSm4Init(t *testing.T) {
 	amd64Expected, _ := hex.DecodeString("efe02875211f104b6cc6398a88e02616832611ffa9ff365d832611ffa9ff365dd1990739ba1568a7b850c2b3d6faa70269c9c58a6cefcfa569c9c58a6cefcfa5c465caca557f2b72b1a41462debd1b0075c1dea88bc2307275c1dea88bc2307285f658150945b9720030ab912a73b71c85c6f38423360e6e85c6f38423360e6e70d7d26d60ba5e2e434c4acffae2f15b339b98a29a58af75339b98a29a58af75edeb6cd41b6c866aa116a5ff33dcbbc04cfdc92b28b03daa4cfdc92b28b03daabf7c2d4efddd55771c7e73c7aa8b732fa3025e8957562658a3025e89575626585444a9b72066aa2e99458213d6e8ef4ccd012ba4f68e4562cd012ba4f68e4562")
 	arm64Expected, _ := hex.DecodeString("6cc6398a88e02616efe02875211f104b832611ffa9ff365d832611ffa9ff365db850c2b3d6faa702d1990739ba1568a769c9c58a6cefcfa569c9c58a6cefcfa5b1a41462debd1b00c465caca557f2b7275c1dea88bc2307275c1dea88bc230720030ab912a73b71c85f658150945b97285c6f38423360e6e85c6f38423360e6e434c4acffae2f15b70d7d26d60ba5e2e339b98a29a58af75339b98a29a58af75a116a5ff33dcbbc0edeb6cd41b6c866a4cfdc92b28b03daa4cfdc92b28b03daa1c7e73c7aa8b732fbf7c2d4efddd5577a3025e8957562658a3025e895756265899458213d6e8ef4c5444a9b72066aa2ecd012ba4f68e4562cd012ba4f68e4562")
 	switch runtime.GOARCH {
-	case "arm64", "riscv64":
+	case "arm64":
 		if !bytes.Equal(table[:], arm64Expected) {
 			t.Errorf("unexpected table value: got %x, want %x", table, arm64Expected)
 		}
-	case "amd64":
+	case "amd64", "riscv64":
 		if !bytes.Equal(table[:], amd64Expected) {
 			t.Errorf("unexpected table value: got %x, want %x", table, amd64Expected)
 		}
@@ -200,11 +200,11 @@ func TestGcmSm4Data(t *testing.T) {
 		copy(arm64Expected[8:], amd64Expected[:8])
 		gcmSm4Data(&table, data, &y)
 		switch runtime.GOARCH {
-		case "arm64", "riscv64":
+		case "arm64":
 			if !bytes.Equal(y[:], arm64Expected) {
 				t.Errorf("case %d: unexpected result: got %x, want %x", i, y, arm64Expected)
 			}
-		case "amd64":
+		case "amd64", "riscv64":
 			if !bytes.Equal(y[:], amd64Expected) {
 				t.Errorf("case %d: unexpected result: got %x, want %x", i, y, amd64Expected)
 			}
