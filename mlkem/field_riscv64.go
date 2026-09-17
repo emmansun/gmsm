2be6cccf4371eb99e5d851a00363482109cb9c69
Sun Yimin <emmansun@users.noreply.github.com>
2026-09-17 16:39:50 +0800

    mlkem: use riscv64 keccakx4

--- mlkem/field_riscv64.go
+++ mlkem/field_riscv64.go
@@ -11,6 +11,7 @@ import (
 	"crypto/sha3"
 
 	"github.com/emmansun/gmsm/internal/deps/cpu"
+	"github.com/emmansun/gmsm/internal/keccakx4"
 )
 
 var hasRVV = cpu.RISCV64.HasV
@@ -195,10 +196,30 @@ func sampleNTT(rho []byte, ii, jj byte) nttElement {
 }
 
 func sampleNTTx4(rho []byte, indices [4][2]byte) [4]nttElement {
-	return [4]nttElement{
-		sampleNTT(rho, indices[0][0], indices[0][1]),
-		sampleNTT(rho, indices[1][0], indices[1][1]),
-		sampleNTT(rho, indices[2][0], indices[2][1]),
-		sampleNTT(rho, indices[3][0], indices[3][1]),
+	var xof keccakx4.SHAKE128x4
+	xof.AbsorbSeed(rho, indices)
+
+	var results [4]nttElement
+	var j [4]int
+	var batch [4][168]byte
+
+	for {
+		xof.Squeeze(batch[0][:], batch[1][:], batch[2][:], batch[3][:])
+		allDone := true
+		for lane := range 4 {
+			if j[lane] >= n {
+				continue
+			}
+			for off := 0; off < 168 && j[lane] < n; off += 24 {
+				j[lane] += rejUniformGeneric(batch[lane][off:off+24], &results[lane], j[lane])
+			}
+			if j[lane] < n {
+				allDone = false
+			}
+		}
+		if allDone {
+			break
+		}
 	}
+	return results
 }
