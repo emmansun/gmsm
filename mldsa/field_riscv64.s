b7f01eab754bfe75a1978edd008504747a43e9b8
Sun Yimin <emmansun@users.noreply.github.com>
2026-09-24 17:41:27 +0800

    mldsa: fix polyInfinityNormRVV

--- mldsa/field_riscv64.s
+++ mldsa/field_riscv64.s
@@ -976,9 +976,10 @@ TEXT ·polyInfinityNormRVV(SB), NOSPLIT, $0-12
 	MOV	a+0(FP), X10
 	MOV	$8380417, Q
 	MOV	$256, X13
-	MOV	$0, X24
 
-	// Reduce each strip-mined chunk and keep the global maximum in X24.
+	// Accumulate max_u(min(a[i], q-a[i])) in V12.
+	VSETVLI X13, E32, M4, TA, MA, X14
+	VMVVI	$0, V12
 
 poly_inf_norm_rvv_loop:
 	VSETVLI X13, E32, M4, TA, MA, X14
@@ -986,30 +987,29 @@ poly_inf_norm_rvv_loop:
 
 	VRSUBVX	Q, V4, V8
 	VMINUVV	V8, V4, V4
-	VMVVI	$0, V16
-	VREDMAXUVS	V4, V16, V20
-	VMVXS	V20, X15
-	SLTU	X24, X15, X16
-	SUB	ZERO, X16, X16
-	XOR	X24, X15, X17
-	AND	X16, X17, X17
-	XOR	X17, X24, X24
+	VMAXUVV	V12, V4, V12
 
 	SLL	$2, X14, X15
 	ADD	X15, X10, X10
 	SUB	X14, X13, X13
 	BNEZ	X13, poly_inf_norm_rvv_loop
 
-	MOVW	X24, ret+8(FP)
+	MOV	$256, X13
+	VSETVLI X13, E32, M4, TA, MA, X14
+	VMVVI	$0, V16
+	VREDMAXUVS	V12, V16, V20
+	VMVXS	V20, X14
+	MOVW	X14, ret+8(FP)
 	RET
 
 // func polyInfinityNormSignedRVV(a *int32) uint32
 TEXT ·polyInfinityNormSignedRVV(SB), NOSPLIT, $0-12
 	MOV	a+0(FP), X10
 	MOV	$256, X13
-	MOV	$0, X24
 
-	// Reduce each strip-mined chunk and keep the global maximum in X24.
+	// Accumulate max_u(abs(a[i])) in V12.
+	VSETVLI X13, E32, M4, TA, MA, X14
+	VMVVI	$0, V12
 
 poly_inf_norm_signed_rvv_loop:
 	VSETVLI X13, E32, M4, TA, MA, X14
@@ -1019,19 +1019,17 @@ poly_inf_norm_signed_rvv_loop:
 	VSRAVI	$31, V4, V8
 	VXORVV	V8, V4, V4
 	VSUBVV	V8, V4, V4
-	VMVVI	$0, V16
-	VREDMAXUVS	V4, V16, V20
-	VMVXS	V20, X15
-	SLTU	X24, X15, X16
-	SUB	ZERO, X16, X16
-	XOR	X24, X15, X17
-	AND	X16, X17, X17
-	XOR	X17, X24, X24
+	VMAXUVV	V12, V4, V12
 
 	SLL	$2, X14, X15
 	ADD	X15, X10, X10
 	SUB	X14, X13, X13
 	BNEZ	X13, poly_inf_norm_signed_rvv_loop
 
-	MOVW	X24, ret+8(FP)
+	MOV	$256, X13
+	VSETVLI X13, E32, M4, TA, MA, X14
+	VMVVI	$0, V16
+	VREDMAXUVS	V12, V16, V20
+	VMVXS	V20, X14
+	MOVW	X14, ret+8(FP)
 	RET
